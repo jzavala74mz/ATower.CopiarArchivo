054cd4caa13a83d0f8c40ec52ca7d7382269b1a0
root <root@atower.vmwarelab>
2021-01-10 17:25:30 -0600

    Ejemplo de copia de archivo

--- ScriptCopyPrueba.sh
+++ ScriptCopyPrueba.sh
@@ -1,8 +1,18 @@
 # spawn scp /path_from/file_name user_name_here@to_host_name:/path_to
 spawn scp root@192.168.112.174:/home/jzavala74mz/Uploads/Apr2006_MDX1_x86_Archive.cab /root/Apr2006_MDX1_x86_Archive.cab
-expect "Are you sure you want to continue connecting (yes/no)?"
-send Yes\n;
-expect "password:"
-send P4ssw0rd\n;
-interact
+expect {
+      "Are you sure you want to continue connecting (yes/no)?" {
+            send "yes\r"
+            exp_continue
+      }
+      "password:" {
+            send "P4ssw0rd\r"
+            interact
+      }
+}
+## expect "Are you sure you want to continue connecting (yes/no)?"
+## send Yes\n;
+## expect "password:"
+## send P4ssw0rd\n;
+## interact
 
